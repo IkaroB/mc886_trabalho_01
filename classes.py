5e6d5d22c3aeb4689fc6ed9d0a9fcedcba3cabb3
IkaroB <i174964@dac.unicamp.br>
2022-03-27 16:56:50 -0300

    First version for the classes used in the problem.

---
+++ classes.py
@@ -0,0 +1,50 @@
+# Exercício 1 de MO416A/MC886B 1s2022
+
+# Este módulo python contém as classes usadas no exercício.
+
+class LineSeg:
+	def __init__(self, v1, v2):
+		self.v1 = v1
+		self.v2 = v2
+
+
+class Vertex:
+	def __init__(self, name, x, y):
+		self.name = name
+		self.coord = Point(x, y)
+		self.visible = []
+		self.belongs_poly = None
+
+	def print(self):
+		print(f"{self.name}: x={self.coord.x}, y={self.coord.y};")
+		if self.visible:
+			print(f"\tVisible:{self.visible}")
+		if self.belongs_poly:
+			print(f"\tBelongs to:{self.belongs_poly.name}\n")
+
+	def belongs_to(self, polygon):
+		self.belongs_poly = polygon
+
+
+class Polygon:
+	def __init__(self, name, *vertices):
+		self.name = name
+		self.vertices = vertices
+		self.concavity = False
+		self.bay = []
+
+	def print(self):
+		print(f"{self.name}:", end="")
+		for vert in self.vertices:
+			print("\t", end="")
+			vert.print()
+		print("\n")
+
+
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def print(self):
+		print(f"x={self.x}, y={self.y};")
